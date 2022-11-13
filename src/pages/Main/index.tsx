4f56fd2121bb2822173eed475e74ff7df5fdb909
BoraParkDev <borapark13@gmail.com>
2022-11-13 12:53:33 +0900

    modify: main.tsx에 공통 컴포넌트 별도 삽입

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -8,6 +8,8 @@ import face from "src/assets/face.svg";
 import chart from "src/assets/chart.svg";
 import chartCup from "src/assets/chartCup.svg";
 import btnArrow from "src/assets/btn_arrow.svg";
+import { HeaderContainer } from "src/components/Header";
+import { FooterContainer } from "src/components/Footer";
 
 export const Main = () => {
   const navigate = useNavigate();
@@ -25,47 +27,51 @@ export const Main = () => {
   console.log(process);
 
   return (
-    <main css={styles.mainWrapper}>
-      <div css={styles.mainStyle}>
-        <div css={styles.graphStyle}>
-          <div css={styles.innerWrapper}>
-            <img src={face} alt="timer" css={styles.faceImgStyle} />
-            <div css={styles.textwrapper}>
-              <p css={styles.textStyle}>오늘 달린 거리</p>
-              <p css={styles.distanceStyle}>
-                4.00<span className="kilometer">km</span>
-              </p>
-              <p css={styles.caloryStyle}>
-                120<span className="kcal">Kcal</span>
-              </p>
+    <>
+      <HeaderContainer />
+      <main css={styles.mainWrapper}>
+        <div css={styles.mainStyle}>
+          <div css={styles.graphStyle}>
+            <div css={styles.innerWrapper}>
+              <img src={face} alt="timer" css={styles.faceImgStyle} />
+              <div css={styles.textwrapper}>
+                <p css={styles.textStyle}>오늘 달린 거리</p>
+                <p css={styles.distanceStyle}>
+                  4.00<span className="kilometer">km</span>
+                </p>
+                <p css={styles.caloryStyle}>
+                  120<span className="kcal">Kcal</span>
+                </p>
+              </div>
             </div>
           </div>
+          <button
+            css={styles.btnCommon}
+            className="btnStart"
+            onClick={() => {
+              navigate("/reward");
+              dispatch(changeProcess("start"));
+            }}
+          >
+            <span css={{ paddingLeft: 28, fontWeight: 700, fontSize: 16 }}>
+              운동 시작하기
+            </span>
+          </button>
         </div>
-        <button
-          css={styles.btnCommon}
-          className="btnStart"
-          onClick={() => {
-            navigate("/reward");
-            dispatch(changeProcess("start"));
-          }}
-        >
-          <span css={{ paddingLeft: 28, fontWeight: 700, fontSize: 16 }}>
-            운동 시작하기
-          </span>
-        </button>
-      </div>
-      <ul css={styles.listWrapper}>
-        <li css={styles.listStyle}>
-          <img src={chart} className="chart" alt="chart" />
-          <p>나의 운동 기록</p>
-          <img src={btnArrow} className="btnArrow" alt="btn" />
-        </li>
-        <li css={styles.listStyle}>
-          <img src={chartCup} className="chart" alt="challenge" />
-          <p>진행 중인 챌린지</p>
-          <img src={btnArrow} className="btnArrow" alt="btn" />
-        </li>
-      </ul>
-    </main>
+        <ul css={styles.listWrapper}>
+          <li css={styles.listStyle}>
+            <img src={chart} className="chart" alt="chart" />
+            <p>나의 운동 기록</p>
+            <img src={btnArrow} className="btnArrow" alt="btn" />
+          </li>
+          <li css={styles.listStyle}>
+            <img src={chartCup} className="chart" alt="challenge" />
+            <p>진행 중인 챌린지</p>
+            <img src={btnArrow} className="btnArrow" alt="btn" />
+          </li>
+        </ul>
+      </main>
+      <FooterContainer />
+    </>
   );
 };
