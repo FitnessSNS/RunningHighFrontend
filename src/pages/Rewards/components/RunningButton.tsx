95c97f9f68c029c70c5f765d243d2922dcb57bde
BoraParkDev <borapark13@gmail.com>
2022-12-12 08:50:20 +0900

    modify: 러닝 상태에 따라 버튼 변경 로직 추가

--- src/pages/Rewards/components/RunningButton.tsx
+++ src/pages/Rewards/components/RunningButton.tsx
@@ -1,25 +1,46 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "../css/runningStyles";
 
+import { useRewardCheck, useRewardStop, useRewardEnd } from "../utils/api";
+import { PositionProps } from "../utils/handleError";
 import { RewardInfoProps } from "../Running";
 
 import play from "src/assets/icon/btn_play.svg";
 import pause from "src/assets/icon/btn_pause.svg";
 import stop from "src/assets/icon/btn_stop.svg";
 import cam from "src/assets/icon/btn_cam.svg";
-import { useAppDispatch } from "src/app/hooks";
 
-export const RunningButton = (props: { rewardsInfo: RewardInfoProps }) => {
-  const dispatch = useAppDispatch();
+export const RunningButton = (props: {
+  position: PositionProps;
+  rewardsInfo: RewardInfoProps;
+  setModalClicked: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const onClickCheck = useRewardCheck(props.position, false);
+  const onClickStop = useRewardStop(props.position);
+  const onClickEnd = useRewardEnd(props.position, false);
 
   return (
     <>
       {
         {
-          play: <div css={styles.btnRoundStyle(play)} onClick={() => {}} />,
-          pause: <div css={styles.btnRoundStyle(pause)} onClick={() => {}} />,
-          stop: <div css={styles.btnRoundStyle(stop)} onClick={() => {}} />,
+          play: (
+            <div
+              css={styles.btnRoundStyle(play)}
+              onClick={() => onClickCheck}
+            />
+          ),
+          pause: (
+            <div
+              css={styles.btnRoundStyle(pause)}
+              onClick={() => {
+                onClickStop.then(() => props.setModalClicked(true));
+              }}
+            />
+          ),
+          stop: (
+            <div css={styles.btnRoundStyle(stop)} onClick={() => onClickEnd} />
+          ),
           cam: <div css={styles.btnRoundStyle(cam)} onClick={() => {}} />,
         }[props.rewardsInfo.status]
       }
